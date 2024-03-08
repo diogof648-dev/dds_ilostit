d25e1ac78d8dba7846b05771a8166f3762a4afad
Diogo da Silva Fernandes <diogosilvafernandes85@gmail.com>
2024-03-08 14:07:02 +0100

    Update tailwind base config to add some details

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,25 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: "#dce2de",
+          200: "#b8c5bc",
+          300: "#95a89b",
+          400: "#718b79",
+          500: "#4e6e58",
+          600: "#3e5846",
+          700: "#2f4235",
+          800: "#1f2c23",
+          900: "#101612"
+},
+      },
+      spacing: {
+        '1/24': '4.15%',
+        '1/10': '10%'
+      }
+    },
   },
   plugins: [],
 };
